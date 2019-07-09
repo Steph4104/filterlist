be1203c8e2326d558781af379d00c83981475acd
steph4104 <stephanie4104@gmail.com>
2019-07-09 17:35:16 -0400

    fix build script

--- babel.config.js
+++ babel.config.js
@@ -1,65 +1,15 @@
-// module.exports = {
-//   presets: [
-//     [
-//       '@babel/preset-env',
-//       {
-//         modules: false,
-//       },
-//     ],
-//     '@babel/preset-react',
-//   ],
-
-//   plugins: [
-//     '@babel/plugin-proposal-class-properties',
-//   ],
-// };
-
 module.exports = {
-  env: {
-    es: {
-      presets: [
-        [
-          '@babel/env', {
-            modules: false,
-          },
-        ],
-        '@babel/react',
-      ],
-    },
-
-    cjs: {
-      presets: [
-        '@babel/env',
-        '@babel/react',
-      ],
-    },
-
-    test: {
-      presets: [
-        '@babel/env',
-        '@babel/react',
-      ],
-    },
-
-    dev: {
-      presets: [
-        [
-          '@babel/env', {
-            modules: false,
-
-            targets: {
-              firefox: '66',
-              chrome: '73',
-            },
-          },
-        ],
-        '@babel/react',
-      ],
-    },
-  },
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: false,
+      },
+    ],
+    '@babel/preset-react',
+  ],
 
   plugins: [
-    '@babel/plugin-transform-runtime',
     '@babel/plugin-proposal-class-properties',
   ],
 };
